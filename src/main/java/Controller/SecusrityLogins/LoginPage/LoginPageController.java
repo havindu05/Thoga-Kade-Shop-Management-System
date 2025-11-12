bffd044fc9add26521edde59318f93aec588af22
Havindu Binduhewa <116324272+havindu05@users.noreply.github.com>
2025-11-12 17:14:59 +0530

    SignUp page UI

--- src/main/java/Controller/SecusrityLogins/LoginPage/LoginPageController.java
+++ src/main/java/Controller/SecusrityLogins/LoginPage/LoginPageController.java
@@ -2,7 +2,13 @@ package Controller.SecusrityLogins.LoginPage;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginPageController {
 
@@ -19,7 +25,16 @@ public class LoginPageController {
     private TextField txtPassword;
 
     @FXML
-    void btnLogin(ActionEvent event) {
+    void btnBack(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/RegisterPage.fxml"));
+        Scene scene = new Scene(loader.load());
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void btnLogin(ActionEvent event) throws IOException {
 
     }
 
