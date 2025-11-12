3ad087c5dad04299c970e9961442f41783e6001a
Havindu Binduhewa <116324272+havindu05@users.noreply.github.com>
2025-11-12 12:24:33 +0530

    Create register page UI

---
+++ src/main/java/Controller/RegisterPage/RegisterPageController.java
@@ -0,0 +1,23 @@
+package Controller.RegisterPage;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class RegisterPageController {
+
+    @FXML
+    void btnGuest(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignUp(ActionEvent event) {
+
+    }
+
+}
